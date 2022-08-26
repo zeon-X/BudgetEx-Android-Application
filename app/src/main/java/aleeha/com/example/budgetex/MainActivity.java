cc4e025211b6e9264ad3a525ac0cc532a777000d
zeon-X <mdshefatzeon@gmail.com>
2022-08-26 14:39:43 +0600

    dashboard done

---
+++ app/src/main/java/aleeha/com/example/budgetex/MainActivity.java
@@ -0,0 +1,14 @@
+package aleeha.com.example.budgetex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
